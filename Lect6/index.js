8cbbaa072feff2e6f7b26528e6f1cd98f249b5bc
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-11 10:44:12 +0530

    idk

--- Lect6/index.js
+++ Lect6/index.js
@@ -12,6 +12,7 @@ app.use((req,res, next)=>{
     console.log(`in the middleware`)
     next()
 })
+
 app.use((req,res, next)=>{
     console.log(`in the another middleware`)
     res.send('<h1>hello from express</h1>');
