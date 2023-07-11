d2e782a96b4c4a4ae35a71b9eb02019330cb75be
slayerrr12 <eyeamarinsharma@gmail.com>
2023-07-11 11:00:41 +0530

    assignment 1 complete

---
+++ Assign1/index.js
@@ -0,0 +1,24 @@
+//importing nativnode modules
+const http = require('http');
+
+
+//importing third party packages
+const express = require("express");
+const app = express();
+
+
+
+app.use((req,res, next)=>{
+    console.log(`in the middleware`)
+    
+})
+
+app.use("/users",(req,res, next)=>{
+    console.log(`in the another middleware`)
+    res.send('<h1>hello from express</h1>');
+})
+
+
+console.log("server is up and listening on port 3000")
+
+app.listen(3000)
